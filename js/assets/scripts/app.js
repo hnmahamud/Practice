214a9a90d9bb72d6a085152c88eb11a32f9e6ed8
hnmahamud <hnmahamud@gmail.com>
2020-03-09 18:56:10 +0600

    attack-strongAttack-heal

---
+++ js/assets/scripts/app.js
@@ -0,0 +1,63 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
+
+function attackMode(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    }
+    else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const monsterDamage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= monsterDamage;
+    endRound();
+}
+
+function attackHandler() {
+    attackMode('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMode('STRONG_ATTACK');
+}
+
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert('You can not heal to more than your max initial health!');
+        healValue = chosenMaxLife - currentPlayerHealth;
+    }
+    else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
