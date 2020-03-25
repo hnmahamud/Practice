93f1a4df19b15a212f730d601fcaafc5526c16e8
hnmahamud <hnmahamud@gmail.com>
2020-03-25 22:45:34 +0600

    innitial commit

--- js/app.js
+++ js/app.js
@@ -18,14 +18,37 @@
 // }
 //
 // console.log(personalData[1]);
-
-const hobbies = ['Cooking' , 'Sports'];
+//
+// const hobbies = ['Cooking' , 'Sports'];
 // hobbies.push('Reading');
 // hobbies.pop();
-hobbies.unshift('Reading');
-hobbies.shift();
-hobbies[1] = 'Coding';
-//hobbies[5] = 'Reading';
-hobbies.splice(0);
-console.log(hobbies);
+// hobbies.unshift('Reading');
+// hobbies.shift();
+// hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading';
+// hobbies.splice(5);
+// console.log(hobbies);
+//
+// const testResult = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storedResult = testResult.slice(0, 2);
+// const storedResult = testResult.concat([3.99, 2]);
+// testResult.push(5.91);
+// console.log(storedResult, testResult);
+// console.log(testResult.indexOf(5.3));
+//
+// const personData = [{name: 'Hasan'}, {name: 'Mahamud'}];
+// for (let i of personData) {
+//     if (i.name === 'Hasan') {
+//         console.log(i.name);
+//         break;
+//     }
+// }
 
+const datas = [1, 2, 3, 4, 5];
+// for (const i of datas) {
+//     console.log(i);
+// }
+
+datas.forEach((data, index) => {
+    console.log(index + ':' + data);
+});
